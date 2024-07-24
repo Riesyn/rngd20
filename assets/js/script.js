f86e86dcd288bc88aa824499af3eafa868e49bf2
Robby <robby@henderson-woods.com>
2024-07-23 20:54:19 -0500

    extended js and adjusted table, and included modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 function addNumberToTable(number) {
-    var tableBody = document.querySelector('#numbersTable tbody');
+    var tableBody = document.querySelector('#rollTable tbody');
     var newRow = tableBody.insertRow();
     var cell = newRow.insertCell();
     cell.textContent = number;
 }
 
-var input = prompt('Enter numbers separated by commas:');
-var numbers = input.split(',').map(function(item) {
-    return parseInt(item.trim(), 10);
-});
+// var input = prompt('Enter numbers separated by commas:');
+// var numbers = input.split(',').map(function (item) {
+//     return parseInt(item.trim(), 10);
+// });
 
-numbers.forEach(function(number) {
+numbers.forEach(function (number) {
     addNumberToTable(number);
 });
